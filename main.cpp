5282c9aec8419970ec33611eb0a2f80e2c63ae0d
James Bremner <james@ravenspoint.com>
2020-02-18 12:25:55 -0500

    Queue processes waiting for core

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <vector>
 #include <map>
+#include <queue>
 
 using namespace std;
 
@@ -20,6 +21,8 @@ enum class eEvent
     arrive,
     complete,
     endSim,
+    coreFreed,
+    coreRequest,
 };
 
 /// Resource request
@@ -60,7 +63,10 @@ public:
     {
         vReqs.push_back( cRequest( r, t ));
     }
-    /// Execute next reuest
+    /// Get current request
+    cRequest& Request();
+
+    /// Move on to next request
     void NextRequest();
 
 private:
@@ -92,10 +98,10 @@ public:
     {
         myProcess.find( id )->second.Add( r, t );
     }
-    /** Execute next process request
+    /** Current Request for process
         @param[in] id
     */
-    void ExecNextRequest( int id );
+    cRequest& Request( int id );
 
 private:
     map< int,cProcess > myProcess;
@@ -121,6 +127,8 @@ public:
     /// Human readable text desribing event as it completes
     string text();
 
+    void execute();
+
 };
 
 /// A schedule of events waiting to happen
@@ -141,54 +149,173 @@ public:
         @return the next event
         The simulation clock will be advanced to the next event
     */
-    cEvent Next( int& Clock )
-    {
-        if( ! mySchedule.size() )
-        {
-            // there are no more event,
-            // so return the simulation end event
-            static cEvent e( eEvent::endSim, -1);
-            return e;
-        }
-        Clock = mySchedule.begin()->first;
-        return mySchedule.begin()->second;
-    }
+    cEvent Next( int& Clock );
+
     /// Remove completed event from schedule
     void Done()
     {
         mySchedule.erase( mySchedule.begin() );
     }
+    string text();
 private:
     /// Upcoming events mapped by their times
     multimap< int,cEvent > mySchedule;
+};
+
+/// The cores
+class cCores
+{
+public:
+    int myCoreCount;
+    int myCoreBusy;
+    cCores()
+        : myCoreCount( 1 )
+        , myCoreBusy( 0 )
+    {
+
+    }
+    void set( int count )
+    {
+        myCoreCount = count;
+    }
+    bool Request()
+    {
+        cout << "Core request " << myCoreBusy << " busy of " << myCoreCount << "\n";
+        if ( myCoreBusy == myCoreCount )
+            return false;
+        myCoreBusy++;
+        return true;
+    }
+    void Free()
+    {
+        if( myCoreBusy <= 0 )
+            throw std::runtime_error("cCore undeflow");
+        myCoreBusy--;
+    }
+};
+
+/// The simuklator
+class cProcessorSimulator
+{
+public:
+    void RequestCore( int pid );
+    void FreeCore();
+    string snapShot();
+private:
+    queue<int> myQueue;
+    cCores myCores;
 };
 
 cProcessTable theProcessTable;
 cSchedule theSchedule;
 int theTime;
 
-void cProcess::NextRequest()
+cProcessorSimulator theSim;
+
+void cProcessorSimulator::RequestCore( int pid )
+{
+    // request a core
+    if( myCores.Request() )
+    {
+        // successful - schedule event when th core will be freed
+        theSchedule.Add(
+            theTime + theProcessTable.Request( pid ).time,
+            cEvent( eEvent::coreFreed, pid ));
+    }
+    else
+    {
+        // no core available - add process to queue
+        cout << "process blocked\n";
+        myQueue.push( pid );
+    }
+}
+
+void cProcessorSimulator::FreeCore()
+{
+    // free a core
+    myCores.Free();
+
+    // check for process waiting for core
+    if( myQueue.empty() )
+        return;
+
+    // id of waiting process
+    int pid = myQueue.front();
+
+    // remove process from queue
+    myQueue.pop();
+
+    // request a core ( must succeed because we just freed a core )
+    myCores.Request();
+
+    // schedule event when the core will be freed
+    theSchedule.Add(
+        theTime + theProcessTable.Request( pid ).time,
+        cEvent( eEvent::coreFreed, pid ));
+
+}
+string cProcessorSimulator::snapShot()
+{
+    stringstream ss;
+    ss << theSchedule.text();
+    ss << myQueue.size() << " processes waiting for core\n";
+    ss << "\n";
+    return ss.str();
+}
+
+cEvent cSchedule::Next( int& Clock )
+{
+    if( ! mySchedule.size() )
+    {
+        // there are no more events,
+        // so return the simulation end event
+        static cEvent e( eEvent::endSim, -1);
+        return e;
+    }
+    Clock = mySchedule.begin()->first;
+    // tell user what has happened
+    cEvent E = mySchedule.begin()->second;
+    return E;
+}
+
+std::string cSchedule::text()
+{
+    stringstream ss;
+    ss << "==Schedule==\n";
+    for( auto it = mySchedule.begin();
+            it != mySchedule.end();
+            it++ )
+    {
+        ss << it->first << " " << it->second.text() << "\n";
+    }
+    ss << "=========\n";
+    return ss.str();
+}
+
+cRequest& cProcess::Request()
 {
     if( myNextReq >= (int)vReqs.size() )
     {
-        return;
+        static cRequest nullreq( eResource::none, -1 );
+        return nullreq;
     }
-
-    cRequest& req = vReqs[ myNextReq++ ];
-    if( req.myRes == eResource::none )
-        return;
-
-
-    // enter the new request into the schedule
-    theSchedule.Add( theTime+req.time, cEvent( eEvent::complete, id ));
-
+    return vReqs[ myNextReq ];
 }
 
-void cProcessTable::ExecNextRequest( int id )
+void cProcess::NextRequest()
 {
-    myProcess.find( id )->second.NextRequest();
-    // remove the completed event from the schedule
-    theSchedule.Done();
+    if( myNextReq < (int)vReqs.size() )
+    {
+        myNextReq++;
+    }
+}
+
+cRequest& cProcessTable::Request( int id )
+{
+    auto it = myProcess.find( id );
+    if( it == myProcess.end() )
+        throw std::runtime_error("lost process");
+    return it->second.Request();
 }
 
 string cEvent::text()
@@ -205,8 +332,41 @@ string cEvent::text()
     case eEvent::endSim:
         ss << "Simulation completed";
         break;
+    case eEvent::coreRequest:
+        ss << "Process " << myPID << " requests core";
+        break;
+    case eEvent::coreFreed:
+        ss << "Process " << myPID << " frees core";
+        break;
     }
     return ss.str();
+}
+
+void cEvent::execute()
+{
+    cout << "Event: " << text() << "\n";
+
+    switch( myType )
+    {
+
+    case eEvent::arrive:
+        switch( theProcessTable.Request( myPID ).myRes )
+        {
+        case eResource::core:
+            theSchedule.Add( theTime, cEvent( eEvent::coreRequest, myPID ));
+            break;
+        }
+        break;
+
+    case eEvent::coreRequest:
+        theSim.RequestCore( myPID );
+        break;
+
+    case eEvent::coreFreed:
+        theSim.FreeCore();
+        break;
+    }
+    theSchedule.Done();
 }
 
 void Read()
@@ -240,6 +400,8 @@ void Read()
 
 void Run()
 {
+    cout << theSim.snapShot();
+
     // start the clock
     theTime = 0;
 
@@ -248,16 +410,16 @@ void Run()
     {
         cEvent E = theSchedule.Next( theTime );
 
-        // tell user what has happened
-        cout << E.text() << " at " << theTime << "\n";
-
         // check for completed simulation
         if( E.myType == eEvent::endSim )
             break;
 
-        // do the process' next request
-        theProcessTable.ExecNextRequest( E.myPID );
+        E.execute();
+
+        cout << theSim.snapShot();
     }
+
+    cout << "Simulation ended at " << theTime << "\n";
 }
 
 int main()
